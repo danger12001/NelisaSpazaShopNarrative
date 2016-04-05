80716b1d9340773236a997bd232f99850c1474b0
danger12001 <paul@projectcodex.co>
2016-04-05 07:25:42 +0200

    codex-version

--- mostPopularCategory.js
+++ mostPopularCategory.js
@@ -1,6 +1,7 @@
+
 var weeklySales = require("./weeklySales");
 var fs = require("fs");
-exports.mostPopularCategory = function(){
+exports.mostPopularCategory = function() {
   var weeklySale = weeklySales.weeklySales();
   var weeklySales1 = weeklySale.week1;
   var weeklySales2 = weeklySale.week2;
@@ -11,8 +12,8 @@ exports.mostPopularCategory = function(){
   category = category.replace("Product,Category\n", "");
   var interimArray = category.split('\n').join(".").split(',').join('.').split(".");
   var categories = [];
-  for(x=0;x<interimArray.length -1;x++){
-    if(x % 2 === 0){
+  for (x = 0; x < interimArray.length - 1; x++) {
+    if (x % 2 === 0) {
       var stockItem = interimArray[x];
       var categorys = interimArray[x + 1];
       // console.log(stockItem);
@@ -26,35 +27,72 @@ exports.mostPopularCategory = function(){
   }
   // console.log(weeklySales1[0].stockItem);
   var oc1 = [];
-  for(x=0;x<categories.length;x++){
-    for(y=0;y<weeklySales1.length;y++){
-      if(weeklySales1[y].stockItem == categories[x].stockItem){
+  for (x = 0; x < categories.length; x++) {
+    for (y = 0; y < weeklySales1.length; y++) {
+      if (weeklySales1[y].stockItem == categories[x].stockItem) {
         var cat = categories[x].category;
-        var item = categories[x].stockItem;
         var amountsold = weeklySales1[y].sold;
-        console.log(amountsold);
-        oc1.push(amountsold + " " + item + " - " + cat);
-    }
-      // console.log(weeklySales1[x]);
-      // console.log(categories[x].stockItem);
+        // console.log(amountsold);
+        oc1.push({amountsold: amountsold,
+          category: cat});
+      }
     }
   }
-  var mpc1 = oc1.sort(function(a, b){
-    return b.amountsold - a.amountsold;
-  });
-  oc1.sort();
-  console.log(oc1);
-  // console.log(mpc1);
+  var mpc1 = oc1.sort(function(a, b) {
+    return parseFloat(b.amountsold) - parseFloat(a.amountsold);
+});
+var MostPopularCategory1 = mpc1[0];
+var oc2 = [];
+for (x = 0; x < categories.length; x++) {
+  for (y = 0; y < weeklySales2.length; y++) {
+    if (weeklySales2[y].stockItem2 == categories[x].stockItem) {
+      var cat = categories[x].category;
+      var amountsold = weeklySales2[y].sold;
+      // console.log(amountsold);
+      oc2.push({amountsold: amountsold,
+        category: cat});
+    }
+  }
+}
+var mpc2 = oc2.sort(function(a, b) {
+  return parseFloat(b.amountsold) - parseFloat(a.amountsold);
+});
+var MostPopularCategory2 = mpc2[0];
 
-  // console.log(test);
-    // var oc1 = [];
-  // for(x=0;x<weeklySales1.length;x++){
-  //   if(weeklySales1[x].stockItem == categories[x].stockItem){
-  //     var match = categories[x].category;
-  //     // console.log(match);
-  //     var oc1 = {stockItem: weeklySales1[x].stockItem, category: match};
-  //     // console.log(oc1);
-  //     return oc1;
-  //   }
-  // }
+var oc3 = [];
+for (x = 0; x < categories.length; x++) {
+  for (y = 0; y < weeklySales3.length; y++) {
+    if (weeklySales3[y].stockItem == categories[x].stockItem) {
+      var cat = categories[x].category;
+      var amountsold = weeklySales3[y].sold;
+      oc3.push({amountsold: amountsold,
+        category: cat});
+    }
+  }
+}
+var mpc3 = oc3.sort(function(a, b) {
+  return parseFloat(b.amountsold) - parseFloat(a.amountsold);
+});
+var MostPopularCategory3 = mpc3[0];
+
+var oc4 = [];
+for (x = 0; x < categories.length; x++) {
+  for (y = 0; y < weeklySales4.length; y++) {
+    if (weeklySales4[y].stockItem == categories[x].stockItem) {
+      var cat = categories[x].category;
+      var item = categories[x].stockItem;
+      var amountsold = weeklySales4[y].sold;
+      // console.log(amountsold);
+      oc4.push({amountsold: amountsold,
+        category: cat});
+    }
+  }
+}
+var mpc4 = oc4.sort(function(a, b) {
+  return parseFloat(b.amountsold) - parseFloat(a.amountsold);
+});
+// console.log(mpc4[0]);
+var MostPopularCategory4 = mpc4[0];
+var mostPopularCategory = "week 1: " + MostPopularCategory1.category + "\n" +"week 2: " + MostPopularCategory2.category+ "\n" +"week 3: " + MostPopularCategory3.category+ "\n" +"week 4: " + MostPopularCategory4.category;
+return mostPopularCategory;
 };
