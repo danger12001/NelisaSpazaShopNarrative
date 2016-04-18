6cd832e63e0906c67add8475aa8784ec4453daa8
danger12001 <paul@projectcodex.co>
2016-04-18 13:11:32 +0200

    Fixed Handlebars

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ var leastPopularProduct = require('../leastPopularProduct');
 var mostPopularCategory = require('../mostPopularCategory');
 var leastPopularCategory = require('../leastPopularCategory');
 var mostProfitableProduct = require('../mostProfitableProduct');
-var leastProfitableProduct = require('../leastProfitableProduct');
+var mostProfitableCategory = require('../mostProfitableCategory');
 var weeklySales = require('../weeklySales');
 
 var week1 = String(fs.readFileSync("../handlebars/week1.hdb"));
@@ -23,22 +23,22 @@ var week4Sales = Sales.week4;
 
 var source1  = week1;
 var template1 = handlebars.compile(source1);
-var context1 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week1, leastPopularProduct: leastPopularProduct.leastPopularProduct().week1, mostPopularCategory: mostPopularCategory.mostPopularCategory().week1,leastPopularCategory: leastPopularCategory.leastPopularCategory().week1,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week1, leastProfitableProduct: leastProfitableProduct.leastProfitableProduct().week1, weeklySales: week1Sales };
+var context1 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week1, leastPopularProduct: leastPopularProduct.leastPopularProduct().week1, mostPopularCategory: mostPopularCategory.mostPopularCategory().week1,leastPopularCategory: leastPopularCategory.leastPopularCategory().week1,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week1, mostProfitableCategory: mostProfitableCategory.mostProfitableCategory().week1, weeklySales: week1Sales };
 var html1    = template1(context1);
 
 var source2  = week2;
 var template2 = handlebars.compile(source2);
-var context2 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week2, leastPopularProduct: leastPopularProduct.leastPopularProduct().week2, mostPopularCategory: mostPopularCategory.mostPopularCategory().week2,leastPopularCategory: leastPopularCategory.leastPopularCategory().week2,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week2, leastProfitableProduct: leastProfitableProduct.leastProfitableProduct().week2, weeklySales: week2Sales };
+var context2 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week2, leastPopularProduct: leastPopularProduct.leastPopularProduct().week2, mostPopularCategory: mostPopularCategory.mostPopularCategory().week2,leastPopularCategory: leastPopularCategory.leastPopularCategory().week2,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week2, mostProfitableCategory: mostProfitableCategory.mostProfitableCategory().week2, weeklySales: week2Sales };
 var html2    = template2(context2);
 //
 var source3  = week3;
 var template3 = handlebars.compile(source3);
-var context3 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week3, leastPopularProduct: leastPopularProduct.leastPopularProduct().week3, mostPopularCategory: mostPopularCategory.mostPopularCategory().week3,leastPopularCategory: leastPopularCategory.leastPopularCategory().week3,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week3, leastProfitableProduct: leastProfitableProduct.leastProfitableProduct().week3, weeklySales: week3Sales };
+var context3 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week3, leastPopularProduct: leastPopularProduct.leastPopularProduct().week3, mostPopularCategory: mostPopularCategory.mostPopularCategory().week3,leastPopularCategory: leastPopularCategory.leastPopularCategory().week3,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week3, mostProfitableCategory: mostProfitableCategory.mostProfitableCategory().week3, weeklySales: week3Sales };
 var html3    = template3(context3);
 //
 var source4  = week4;
 var template4 = handlebars.compile(source4);
-var context4 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week4, leastPopularProduct: leastPopularProduct.leastPopularProduct().week4, mostPopularCategory: mostPopularCategory.mostPopularCategory().week4,leastPopularCategory: leastPopularCategory.leastPopularCategory().week4,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week4, leastProfitableProduct: leastProfitableProduct.leastProfitableProduct().week4, weeklySales: week4Sales };
+var context4 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week4, leastPopularProduct: leastPopularProduct.leastPopularProduct().week4, mostPopularCategory: mostPopularCategory.mostPopularCategory().week4,leastPopularCategory: leastPopularCategory.leastPopularCategory().week4,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week4, mostProfitableCategory: mostProfitableCategory.mostProfitableCategory().week1, weeklySales: week4Sales };
 var html4    = template4(context4);
 
 
