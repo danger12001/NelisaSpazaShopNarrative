5d8af9d11d7c7572357a47fcecc763fde4168806
danger12001 <paul@projectcodex.co>
2016-04-25 06:15:55 +0200

    Installed and setup heroku

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ var dbOptions = {
       user: 'root',
       password: '5550121a',
       port: 3000,
-      database: 'Nelisa'
+      // database: 'Nelisa'
     };
+
 
 
 app.use(myConnection(mysql, dbOptions, 'single'));
@@ -41,6 +42,20 @@ var week2 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week2, 
 var week3 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week3, leastPopularProduct: leastPopularProduct.leastPopularProduct().week3, mostPopularCategory: mostPopularCategory.mostPopularCategory().week3,leastPopularCategory: leastPopularCategory.leastPopularCategory().week3,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week3, mostProfitableCategory: mostProfitableCategory.mostProfitableCategory().week3, weeklySales: week3Sales };
 var week4 = {mostPopularProduct: mostPopularProduct.mostPopularProduct().week4, leastPopularProduct: leastPopularProduct.leastPopularProduct().week4, mostPopularCategory: mostPopularCategory.mostPopularCategory().week4,leastPopularCategory: leastPopularCategory.leastPopularCategory().week4,mostProfitableProduct: mostProfitableProduct.mostProfitableProduct().week4, mostProfitableCategory: mostProfitableCategory.mostProfitableCategory().week1, weeklySales: week4Sales };
 
+// connection.query('CREATE TABLE people(id int primary key, name varchar(255), age int, address text)', function(err, result) {
+//     if (err) throw err;
+//     connection.query('INSERT INTO people (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA'], function(err, result) {
+//       if (err) throw err;
+//       connection.query('SELECT * FROM people', function(err, results) {
+//         if (err) throw err;
+//         console.log(results[0].id);
+//         console.log(results[0].name);
+//         console.log(results[0].age);
+//         console.log(results[0].address);
+//             }
+//           );
+//         });
+      // });
 // create a route
 app.get('/', function (req, res) {
  res.render("index");
